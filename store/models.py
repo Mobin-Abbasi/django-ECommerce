db0112f5d91201f76a5c829935c98d97ff85a846
Mobin <mobin04dev@gmail.com>
2024-03-05 22:49:13 +0330

    add 4 models feature and coding admin

--- store/models.py
+++ store/models.py
@@ -1,3 +1,50 @@
 from django.db import models
+import datetime
+
 
 # Create your models here.
+
+# Categories of Products
+class Category(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+# Customer
+class Customer(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone = models.CharField(max_length=11)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'
+
+
+# All of our products
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products', default=1)
+    price = models.DecimalField(default=0, decimal_places=2, max_digits=6)
+    description = models.CharField(max_length=255, blank=True, null=True, default='')
+    image = models.ImageField(upload_to='uploads/product')
+
+    def __str__(self):
+        return self.name
+
+
+# Customer Order
+class Order(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='orders')
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name='orders')
+    quantity = models.PositiveIntegerField(default=1)
+    address = models.CharField(max_length=255, default='', blank=True)
+    phone = models.CharField(max_length=11, default='', blank=True)
+    date = models.DateField(default=datetime.datetime.now)
+    status = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.product
