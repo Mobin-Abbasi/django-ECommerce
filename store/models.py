b1c27cc07871cc9a8618e1763414d3b7c6094e56
Mobin <mobin04dev@gmail.com>
2024-03-07 21:57:05 +0330

    coding product template

--- store/models.py
+++ store/models.py
@@ -8,6 +8,9 @@ import datetime
 class Category(models.Model):
     name = models.CharField(max_length=50)
 
+    class Meta:
+        verbose_name_plural = 'Categories'
+
     def __str__(self):
         return self.name
 
--- store/views.py
+++ store/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
 
 
 def home(request):
+    products = Product.objects.all()
     context = {
-
+        'products': products
     }
     return render(request, 'home.html', context)
