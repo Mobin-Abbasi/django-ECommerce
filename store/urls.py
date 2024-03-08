cd822724d4c69483faee4037195960a805a49866
Mobin <mobin04dev@gmail.com>
2024-03-08 16:57:54 +0330

    add base, navbar, about template and coding view, urls for about

--- store/urls.py
+++ store/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'store'
 
 urlpatterns = [
-    path('', views.home, name='home')
+    path('', views.home, name='home'),
+    path('about', views.about, name='about'),
 ]
--- store/views.py
+++ store/views.py
@@ -10,3 +10,7 @@ def home(request):
         'products': products
     }
     return render(request, 'home.html', context)
+
+
+def about(request):
+    return render(request, 'about.html', {})
