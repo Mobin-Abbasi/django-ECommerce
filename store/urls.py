f7c657a481e4364f0bfd486c19452c690122dcfd
Mobin <mobin04dev@gmail.com>
2024-03-08 21:29:04 +0330

    add login, logout feature

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,6 @@ app_name = 'store'
 urlpatterns = [
     path('', views.home, name='home'),
     path('about', views.about, name='about'),
+    path('login', views.login_user, name='login'),
+    path('logout', views.logout_user, name='logout'),
 ]
--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import *
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 
@@ -14,3 +16,25 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html', {})
+
+
+def login_user(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You have been logged in')
+            return redirect('store:home')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('store:login')
+    else:
+        return render(request, 'login.html', {})
+
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('store:home')
