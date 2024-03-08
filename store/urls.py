b235214d33d4f77f4659a78a8e5f9102c8f167b7
Mobin <mobin04dev@gmail.com>
2024-03-08 23:31:11 +0330

    add register feature

--- store/urls.py
+++ store/urls.py
@@ -5,7 +5,8 @@ app_name = 'store'
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('about', views.about, name='about'),
-    path('login', views.login_user, name='login'),
-    path('logout', views.logout_user, name='logout'),
+    path('about/', views.about, name='about'),
+    path('login/', views.login_user, name='login'),
+    path('logout/', views.logout_user, name='logout'),
+    path('register/', views.register_user, name='register'),
 ]
--- store/views.py
+++ store/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render, redirect
 from .models import *
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django import forms
+from .forms import *
 # Create your views here.
 
 
@@ -38,3 +41,23 @@ def logout_user(request):
     logout(request)
     messages.success(request, 'You have been logged out')
     return redirect('store:home')
+
+
+def register_user(request):
+    form = SignUpForm()
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            # log in user
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            messages.success(request, 'You have been registered and logged in!!! Welcome!!')
+            return redirect('store:home')
+        else:
+            messages.error(request, 'Invalid username or password')
+            return redirect('store:register')
+    else:
+        return render(request, 'register.html', {'form': form})
