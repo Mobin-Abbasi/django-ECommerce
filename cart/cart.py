db353205e2dab47242729e9b84b0f157ff49197d
Mobin <mobin04dev@gmail.com>
2024-03-10 23:13:54 +0330

    add add product in cart ajax but have little bug

--- cart/cart.py
+++ cart/cart.py
@@ -11,4 +11,13 @@ class Cart():
 
         # Make sure cart is available on all page of site
         self.cart = cart
-        
+
+    def add(self, product):
+        product_id = str(product.id)
+        # Logic
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)}
+
+        self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import *
+from django.shortcuts import render, get_object_or_404
+from .cart import Cart
+from store.models import Product
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -10,7 +12,19 @@ def cart_summary(request):
 
 
 def cart_add(request):
-    pass
+    # Get The Cart
+    cart = Cart(request)
+    # test for POST
+    if request.POST.get('action') == 'post':
+        # Get stuff
+        product_id = int(request.POST.get('product_id'))
+        # lookup product in DB
+        product = get_object_or_404(Product, id=product_id)
+        # Save to session
+        cart.add(product=product)
+        # Return response
+        response = JsonResponse({'Product Name': product.name})
+        return response
 
 
 def cart_update(request):
