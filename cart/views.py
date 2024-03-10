f7615e89c33877436351172f5753d0a6289a3f67
Mobin <mobin04dev@gmail.com>
2024-03-10 23:38:20 +0330

    update add cart to product view, ajax and fix bug

--- cart/views.py
+++ cart/views.py
@@ -22,8 +22,10 @@ def cart_add(request):
         product = get_object_or_404(Product, id=product_id)
         # Save to session
         cart.add(product=product)
+        # Get Cart Quantity
+        cart_quantity = cart.__len__()
         # Return response
-        response = JsonResponse({'Product Name': product.name})
+        response = JsonResponse({'qty': cart_quantity})
         return response
 
 
