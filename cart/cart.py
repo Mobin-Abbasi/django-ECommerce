c2272d929ba440e1b66df9e69d7247fa5c2c343b
Mobin <mobin04dev@gmail.com>
2024-03-11 20:36:31 +0330

    Product Quantities

--- cart/cart.py
+++ cart/cart.py
@@ -14,13 +14,14 @@ class Cart():
         # Make sure cart is available on all page of site
         self.cart = cart
 
-    def add(self, product):
+    def add(self, product, quantity):
         product_id = str(product.id)
+        product_qty = str(quantity)
         # Logic
         if product_id in self.cart:
             pass
         else:
-            self.cart[product_id] = {'price': str(product.price)}
+            self.cart[product_id] = int(product_qty)
 
         self.session.modified = True
 
@@ -34,3 +35,7 @@ class Cart():
         products = Product.objects.filter(id__in=product_ids)
         # return those looked uo products
         return products
+
+    def get_quants(self):
+        quantities = self.cart
+        return quantities
