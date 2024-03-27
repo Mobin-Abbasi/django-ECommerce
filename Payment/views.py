7386af1f11636906bf4933753027767cc1e628c3
Mobin <mobin04dev@gmail.com>
2024-03-27 22:30:44 +0330

    Update Checkout View and Template

--- Payment/views.py
+++ Payment/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from cart.cart import Cart
+from .forms import ShippingForm
+from .models import ShippingAddress
 
 
 # Create your views here.
@@ -13,9 +15,26 @@ def checkout(request):
     cart_products = cart.get_prods()
     quantities = cart.get_quants()
     totals = cart.cart_total()
-    context = {
-        'cart_products': cart_products,
-        'quantities': quantities,
-        'totals': totals
-    }
-    return render(request, 'payment/checkout.html', context)
+    if request.user.is_authenticated:
+        # Checkout as logged in user
+        # Shipping User
+        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        # Sipping Form
+        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        context = {
+            'cart_products': cart_products,
+            'quantities': quantities,
+            'totals': totals,
+            'shipping_form': shipping_form,
+        }
+        return render(request, "payment/checkout.html", context)
+    else:
+        # Checkout as guest
+        shipping_form = ShippingForm(request.POST or None)
+        context = {
+            'cart_products': cart_products,
+            'quantities': quantities,
+            'totals': totals,
+            'shipping_form': shipping_form,
+        }
+        return render(request, 'payment/checkout.html', context)
