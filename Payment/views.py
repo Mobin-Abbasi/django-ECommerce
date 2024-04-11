ed532c66c2def4dedca7d236c366c480e16b9973
Mobin <mobin04dev@gmail.com>
2024-04-11 19:47:09 +0330

    billing info

--- Payment/views.py
+++ Payment/views.py
@@ -1,7 +1,7 @@
 from django.contrib import messages
 from django.shortcuts import render, redirect
 from cart.cart import Cart
-from .forms import ShippingForm
+from .forms import ShippingForm, PaymentForm
 from .models import ShippingAddress
 
 
@@ -20,9 +20,9 @@ def checkout(request):
     if request.user.is_authenticated:
         # Checkout as logged in user
         # Shipping User
-        shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
+        # shipping_user = ShippingAddress.objects.get(user__id=request.user.id)
         # Sipping Form
-        shipping_form = ShippingForm(request.POST or None, instance=shipping_user)
+        shipping_form = ShippingForm(request.POST)
         context = {
             'cart_products': cart_products,
             'quantities': quantities,
@@ -43,33 +43,37 @@ def checkout(request):
 
 
 def billing_info(request):
-    # if request.method == 'POST':
-    # Get the cart
-    cart = Cart(request)
-    cart_products = cart.get_prods
-    quantities = cart.get_quants
-    totals = cart.cart_total()
-    shipping_form = request.POST
-    # Check to See if user is Logged in
-    # if request.user.is_authenticated:
-    #     context = {
-    #         'cart_products': cart_products,
-    #         'quantities': quantities,
-    #         'totals': totals,
-    #         'shipping_info': request.POST,
-    #     }
-    #     return render(request, 'payment/billing_info.html', context)
-    # else:
-    #     # Not Logged in
-    #     pass
-    context = {
-        'cart_products': cart_products,
-        'quantities': quantities,
-        'totals': totals,
-        'shipping_form': shipping_form,
-        'shipping_info': request.POST,
-    }
-    return render(request, 'payment/billing_info.html', context)
-    # else:
-    #     messages.success(request, 'Access Denied')
-    #     return redirect('store:home')
+    if request.method == 'POST':
+        # Get the cart
+        cart = Cart(request)
+        cart_products = cart.get_prods
+        quantities = cart.get_quants
+        totals = cart.cart_total()
+        # Check to See if user is Logged in
+        if request.user.is_authenticated:
+            # Get The Billing Form
+            billing_form = PaymentForm()
+            context = {
+                'cart_products': cart_products,
+                'quantities': quantities,
+                'totals': totals,
+                'shipping_info': request.POST,
+                'billing_form': billing_form,
+            }
+            return render(request, 'payment/billing_info.html', context)
+        else:
+            # Not Logged in
+            # Get The Billing Form
+            billing_form = PaymentForm()
+            pass
+        context = {
+            'cart_products': cart_products,
+            'quantities': quantities,
+            'totals': totals,
+            'shipping_info': request.POST,
+            'billing_form': billing_form,
+        }
+        return render(request, 'payment/billing_info.html', context)
+    else:
+        messages.success(request, 'Access Denied')
+        return redirect('store:home')
