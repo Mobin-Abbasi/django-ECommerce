a5ea14196357a9b18caddf728c27b14b3d3441d9
Mobin <mobin04dev@gmail.com>
2024-05-14 20:30:31 +0330

    Update paymant admin

--- Payment/admin.py
+++ Payment/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from .models import *
+from django.contrib.auth.models import User
 
 # Register your models here.
 
@@ -9,3 +10,23 @@ admin.site.register(ShippingAddress)
 admin.site.register(Order)
 admin.site.register(OrderItem)
 
+
+# Create an OrderItems Inline
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 0
+
+
+# Extend ore Order Model
+class OrderAdmin(admin.ModelAdmin):
+    model = Order
+    readonly_fields = ["date_ordered"]
+    fields = ["user", "full_name", "email", "shipping_address", "amount_paid", "date_ordered"]
+    inlines = [OrderItemInline]
+
+
+# Unregister Order Model
+admin.site.unregister(Order)
+
+# Re-Register our Order AND OrderAdmin
+admin.site.register(Order, OrderAdmin)
