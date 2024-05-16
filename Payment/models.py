812ce02fb0a48eb18d2993f34222d115c3a6ad57
Mobin <mobin04dev@gmail.com>
2024-05-16 18:55:53 +0330

    auto add shipping date

--- Payment/models.py
+++ Payment/models.py
@@ -54,11 +54,21 @@ class Order(models.Model):
         return f'order - {str(self.id)}'
 
 
+# Auto Add shipping Date
+@receiver(pre_save, sender=Order)
+def set_shipped_date_on_update(sender, instance, **kwargs):
+    if instance.pk:
+        now = datetime.datetime.now()
+        obj = sender._default_manager.get(pk=instance.pk)
+        if instance.shipped and not obj.shipped:
+            instance.date_shipped = now
+
+
 # Create Order Items Model
 class OrderItem(models.Model):
     # Foreign Keys
-    order = models.ForeignKey(Order, on_delete=models.CASCADE,  null=True)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE,  null=True)
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, null=True)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, null=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
     quantity = models.PositiveBigIntegerField(default=1)
     price = models.DecimalField(max_digits=7, decimal_places=2)
