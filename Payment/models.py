134e1b811a8cb0b6f3609daef842e902414a5026
Mobin <mobin04dev@gmail.com>
2024-05-16 18:35:10 +0330

    import pre_save, receiver and datetime in payment model

--- Payment/models.py
+++ Payment/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import User
 from store.models import Product
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+import datetime
 
 
 # Create your models here.
